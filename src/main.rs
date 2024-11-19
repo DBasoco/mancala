1c7013269885f4ba735604af516bf1fe1a48dc1c
DBasoco <dbasoco97@gmail.com>
2024-11-19 16:14:48 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+use std::collections::HashMap;
+
+struct Board {
+    rank: HashMap<String, >
+    // seeds: i32,
+    // hole: i32,
+    // player: i32,
+    // mancala: bool
+    //next: Option<Box<Board>>
+}
+
+impl Board {
+    fn new() -> Self {
+        let players = [0, 1];
+        let pits = [0, 1, 2, 3, 4, 5, 6];
+
+        for player in players {
+            for pit in pits {
+                let 
+            }
+        }
+    }
+}
+
+fn main() {
+    let _board = Board::new();
+}
